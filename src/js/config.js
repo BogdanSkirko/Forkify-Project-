e250e2e9c5592d4b44e8037236d76108928247b9
BogdanSkirko <BogdanSkirko@gmail.com>
2022-08-26 12:12:13 +0300

    refactoring Object Data

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 100000;
 export const RES_Per_PAGE = 10;
+export const KEY = 'ecdddbff-a92f-45f6-a352-d5a722e4787a';
